46bdf08a4fe42b65f21e9ad37641f73e025be531
Abdul wahab <bsef19m545@pucit.edu.pk>
2022-05-21 11:17:50 +0500

    Learning app Assignment lesson module lessons from A to E added

--- LearningApp/app/src/main/java/com/example/learningapp/Lessons.java
+++ LearningApp/app/src/main/java/com/example/learningapp/Lessons.java
@@ -14,15 +14,19 @@ public class Lessons extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons);
 
-        Button btn_A,btn_B,btn_C ;
+        Button btn_A,btn_B,btn_C,btn_D,btn_E ;
 
         btn_A=findViewById(R.id.A);
         btn_B=findViewById(R.id.B);
         btn_C=findViewById(R.id.C);
+        btn_D=findViewById(R.id.D);
+        btn_E=findViewById(R.id.E);
 
         btn_A.setOnClickListener(this);
         btn_B.setOnClickListener(this);
         btn_C.setOnClickListener(this);
+        btn_D.setOnClickListener(this);
+        btn_E.setOnClickListener(this);
         Intent intent;
     }
 
@@ -43,6 +47,14 @@ public class Lessons extends AppCompatActivity implements View.OnClickListener{
                 break;
             case  R.id.C:
                 intent=new Intent(Lessons.this,C.class);
+                startActivity(intent);
+                break;
+            case  R.id.D:
+                intent=new Intent(Lessons.this,D.class);
+                startActivity(intent);
+                break;
+            case  R.id.E:
+                intent=new Intent(Lessons.this,E.class);
                 startActivity(intent);
                 break;
 
