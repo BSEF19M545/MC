9c71c97fb126dd08e0dbcd27f98ec98240704cdb
Abdul wahab <bsef19m545@pucit.edu.pk>
2022-06-01 07:16:45 +0500

    custom list view practice

---
+++ customListViewPractice/app/src/main/java/com/example/customlistview_practice/MyListView.java
@@ -0,0 +1,54 @@
+package com.example.customlistview_practice;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+    public class MyListView extends ArrayAdapter<Student> {
+        public MyListView(@NonNull Context context, ArrayList<Student> studentArrayList) {
+            //super(context, R.layout.customized_layout,mainTitle);
+            super(context, 0, studentArrayList);
+            /*this.context = context;
+            this.mainTitle = mainTitle;
+            this.subTitle = subTitle;
+            this.imageId = imageId;*/
+        }
+
+        /*private final Activity context;
+        private final String[] mainTitle;
+        private final String[] subTitle;
+        private final Integer[] imageId;*/
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            Student student = getItem(position);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.customized_layout, parent, false);
+            TextView textViewName = convertView.findViewById(R.id.textViewName);
+            TextView textViewCampus = convertView.findViewById(R.id.textViewCampus);
+            TextView textViewId = convertView.findViewById(R.id.textViewID);
+            ImageView imageView = convertView.findViewById(R.id.imageView);
+            textViewName.setText(student.name);
+            textViewCampus.setText(student.campus);
+            textViewId.setText(student.rollNumber);
+            imageView.setImageResource(student.imageID);
+            return convertView;
+        };
+
+    }
+
+
---
+++ customListViewPractice/app/src/main/java/com/example/customlistview_practice/Student.java
@@ -0,0 +1,13 @@
+package com.example.customlistview_practice;
+
+public class Student {
+    public String name;
+    public String campus;
+    public String rollNumber;
+    public int imageID;
+    public Student(String name, String campus, String rollNumber,int imageID) {
+        this.name = name;
+        this.campus = campus;
+        this.rollNumber = rollNumber;
+        this.imageID = imageID;
+    }}
